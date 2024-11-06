3e76cc5226772f7bc6f8d04ff3e6434f3e0165c1
pheobeayo <pheobeayo@gmail>
2024-11-06 13:35:08 +0100

    fix modal

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -139,7 +139,7 @@ const ProductDetails = () => {
               <Dialog
                 open={isOpen}
                 onClose={() => setIsOpen(false)}
-                className="relative z-50"
+                className="relative z-30"
               >
                 <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
                   <DialogPanel className="max-w-lg space-y-4 border bg-black/60 p-12 rounded-xl">
